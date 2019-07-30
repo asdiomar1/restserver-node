541dfee729491ebe1c5227f1cfd86aea81c90529
Andres <asdiomar1@gmail.com>
2019-07-29 23:02:30 -0300

    Correcciones 4

--- server/server.js
+++ server/server.js
@@ -10,21 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
-let mongooseOptions = {
-    useNewUrlParser: true,
-    useFindAndModify: false
-}
-
-// mongoose.connect(process.env.URLDB, mongooseOptions, (err, res) => {
-
-//     if (err) throw err;
-
-//     console.log('Base de datos ONLINE');
-
-// });
-
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true });
-var db = mongoose.connection;
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true })
+    .then(() => {
+        console.log("URL de coneccion: ", process.env.URLDB);
+        console.log("Base de datos ONLINE");
+    })
+    .catch((err) => {
+        console.error(`Mongoose connection error: ${err}`);
+    });
 
 // mongoose.connect(process.env.URLDB, mongooseOptions)
 //     .then(() => {
